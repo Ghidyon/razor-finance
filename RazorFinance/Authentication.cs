536c21167302c5abd3e7eba52b13dda9d4012b0e
Ghidyon <gideonakunana@gmail.com>
2021-08-01 22:21:47 +0300

    added syntactic sugar

--- RazorFinance/Authentication.cs
+++ RazorFinance/Authentication.cs
@@ -11,22 +11,13 @@ namespace RazorFinance
         private static readonly IEnumerable<Account> _accounts = Database.Accounts();
         private static readonly IEnumerable<Credential> _credentials = Database.Credentials();
 
-        public static async Task<List<Account>> RetrieveAccountDataAsync(string username)
-        {
-            List<Account> accountData = await Task.Run(() => _accounts.Where(data => data.Username.Equals(username.ToLower())).ToList());
-            return accountData;
-        }
+        public static async Task<List<Account>> RetrieveAccountDataAsync(string username) =>
+            await Task.Run(() => _accounts.Where(data => data.Username.Equals(username.ToLower())).ToList());
 
-        public static async Task<List<Credential>> GetUserCredentialsAsync(string username)
-        {
-            List<Credential> userCredentials = await Task.Run(() => _credentials.Where(data => data.Username.Equals(username.ToLower())).ToList());
-            return userCredentials;
-        }
+        public static async Task<List<Credential>> GetUserCredentialsAsync(string username) =>
+            await Task.Run(() => _credentials.Where(data => data.Username.Equals(username.ToLower())).ToList());
 
-        public static async Task<bool> IsValidatedAsync(List<Credential> userCredentials, string password)
-        {
-            bool isValidated = await Task.Run(() => userCredentials?.Any(data => data.Password.Equals(password)) ?? false);
-            return isValidated;
-        }
+        public static async Task<bool> IsValidatedAsync(List<Credential> userCredentials, string password) =>
+            await Task.Run(() => userCredentials?.Any(data => data.Password.Equals(password)) ?? false);
     }
 }
